6cb5049b60328b99a3a3c1090b3dca9c148aaf60
AlikBerry <40476895+AlikBerry@users.noreply.github.com>
2019-11-09 21:04:50 +0300

    Update urls.py

--- docbiz_app/urls.py
+++ docbiz_app/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path("base/", views.base, name='base'),
     path("table_trans/", views.table_trans, name='table_trans'),
-    path("table_trans/filter", views.table_trans_filter, name='table_trans_filter'),
     path("table_employee/", views.employee, name='emp_salary'),
     path("table_clients/", views.clients, name='clients'),
     path("table_cashboxes/", views.cashboxes, name='cashboxes'),
