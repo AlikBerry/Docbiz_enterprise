0ae10fa90f91aa2225c034d9438df831509488fe
AlikBerry <sahibalekberli@gmail.com>
2020-02-05 14:54:07 +0300

    new_updates_admin_settings

--- docbiz/settings.py
+++ docbiz/settings.py
@@ -91,26 +91,26 @@ WSGI_APPLICATION = 'docbiz.wsgi.application'
 # }
 
 
-# DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#          'NAME': 'docbiz_db',
-#          'USER': 'postgres',
-#          'PASSWORD': 'sudo180',
-#          'HOST': 'localhost',
-#          'PORT': '5432',
-#      }
-#  }
-
-
-
-
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': 'docbiz_db',
+         'USER': 'postgres',
+         'PASSWORD': 'sudo180',
+         'HOST': 'localhost',
+         'PORT': '5432',
+     }
+ }
+
+
+
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+# }
 
 
 
