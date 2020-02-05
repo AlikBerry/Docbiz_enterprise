c83271476833d9f40525ea579b339e80c8051de9
AlikBerry <sahibalekberli@gmail.com>
2020-02-05 18:58:44 +0300

    new_updates_admin_settings

--- docbiz/settings.py
+++ docbiz/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'material.admin',
-    'material.admin.default',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
